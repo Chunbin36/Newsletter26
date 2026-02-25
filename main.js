c20f1ae0b06d2d765371f431fae0d499a0c9bfbb
Habeen Lee <your_emailcropvice@icloud.com>
2026-02-25 11:43:29 +0900

    2nd commit

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const Api_KEY=`1762ea44d88e4718ba27607148b2bced`;
 let news = []
 const getLatesNews = async () => {
-    const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${Api_KEY}`);
+    const url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr`);
     const response = await fetch(url);
     const data = await response.json();
     news= data.articles;
